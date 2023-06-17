fb012601665b204dca36742a4fdd63f3c3a2b37a
arusafa <arusafacollege@gmail.com>
2023-06-17 02:57:56 -0400

    ProductDetail page has been updated

--- src/pages/ProductDetailPage.vue
+++ src/pages/ProductDetailPage.vue
@@ -1,9 +1,25 @@
 <template>
-    <h1>Product Detail Page</h1>
+    <div class="img-wrap">
+      <img :src="product.imageName" />
+    </div>
+    <div class="product-details">
+      <h1>{{ product.name }}</h1>
+      <h3 class="price">{{ product.price }}</h3>
+      <button class="add-to-cart">Add to Cart</button>
+    </div>
 </template>
 
 <script>
+import { products } from '../temp-data';
 export default {
   name: 'ProductDetailPage',
+  data() {
+    return {
+      product: products.find(
+        product => product.id === this.$route.params.productId
+        // this.$route.params.productId is the value of the dynamic segment in the URL comes from main.js (Route: /products/:productId)
+      ),
+    }
+  }
 }
 </script>

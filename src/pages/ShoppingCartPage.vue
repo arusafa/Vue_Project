b9c070931522a39e6fe3230fff1adfe8084d350f
arusafa <arusafacollege@gmail.com>
2023-06-17 03:17:44 -0400

    ShoppingCart has been created

--- src/pages/ShoppingCartPage.vue
+++ src/pages/ShoppingCartPage.vue
@@ -1,9 +1,33 @@
 <template>
-    <h1>Shopping Cart Page</h1>
+  <h1>My Shopping Car</h1>
+  <!--using v-if statement if theres no item-->
+  <div v-if="cartItems.length > 0">
+    <div class="product-container" v-for="product in cartItems" :key="product.id">
+      <img class="product-image" :src="product.imageName" />
+      <div class="details-wrap">
+        <h3>{{ product.name }}</h3>
+        <p> {{ product.price }}</p>
+      </div>
+      <button class="remove-button">Remove from Cart</button>
+    </div>
+    <button class="checkout-button">Proceed to Checkout</button>
+  </div>
+  <div v-if="cartItems.length === 0">
+    <h3>There are no items in your cart</h3>
+  </div>
 </template>
 
 <script>
+
+import { cartItems } from '../temp-data';
+
+
 export default {
   name: 'ShoppingCartPage',
+  data() {
+    return {
+      cartItems,
+    }
+  }
 }
 </script>

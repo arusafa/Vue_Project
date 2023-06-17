7cac06b0e0a1874200a085070e0a397aeced3726
arusafa <arusafacollege@gmail.com>
2023-06-17 04:37:39 -0400

    ShoppingCart list has been added

--- src/pages/ShoppingCartPage.vue
+++ src/pages/ShoppingCartPage.vue
@@ -2,14 +2,7 @@
   <h1>My Shopping Cart</h1>
   <!--using v-if statement if theres no item-->
   <div v-if="cartItems.length > 0">
-    <div class="product-container" v-for="product in cartItems" :key="product.id">
-      <img class="product-image" :src="product.imageName" />
-      <div class="details-wrap">
-        <h3>{{ product.name }}</h3>
-        <p> {{ product.price }}</p>
-      </div>
-      <button class="remove-button">Remove from Cart</button>
-    </div>
+    <ShoppingCartList :products="cartItems" />
     <button class="checkout-button">Proceed to Checkout</button>
   </div>
   <div v-if="cartItems.length === 0">
@@ -19,11 +12,15 @@
 
 <script>
 
+import ShoppingCartList from '../components/ShoppingCartList.vue';
 import { cartItems } from '../temp-data';
 
 
 export default {
   name: 'ShoppingCartPage',
+  components: {
+    ShoppingCartList,
+  },
   data() {
     return {
       cartItems,

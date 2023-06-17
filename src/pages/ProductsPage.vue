6a45279952dccad387f27c4719bfd02c14387b71
arusafa <arusafacollege@gmail.com>
2023-06-17 03:53:53 -0400

    ViewDetails button has updated to be linked

--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -1,23 +1,24 @@
 <template>
-    <h1>Products</h1>
-    <div class="grid-wrap">
-      <!--in order to display a list in Vue ==> "v-for directive"-->
-      <div
-        class="product-item"
-        v-for="product in products"
-        :key="product.id"
-        >
-        <img :src="product.imageName" />
-        <h3 class="product-name">{{ product.name }}</h3>
-        <p class="product-price">{{ product.price }}</p>
+  <h1>Products</h1>
+  <div class="grid-wrap">
+    <!--in order to display a list in Vue ==> "v-for directive"-->
+    <div class="product-item" v-for="product in products" :key="product.id">
+      <img :src="product.imageName" />
+      <h3 class="product-name">{{ product.name }}</h3>
+      <p class="product-price">{{ product.price }}</p>
+      <!-- in order to select the id we have to add : before the to-->
+      <!--<router-link :to="`/products/${product.id}`"> -->
+      <router-link :to="/products/ + product.id">
         <button>View Details</button>
-      </div>
+      </router-link>
+
     </div>
+  </div>
 </template>
 
 <script>
 
-import { products }  from '../temp-data'
+import { products } from '../temp-data'
 
 export default {
   name: 'ProductsPage',

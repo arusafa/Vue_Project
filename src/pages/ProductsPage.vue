a69ac5759ed912718aefe0d3f6dfdf963d495d36
arusafa <arusafacollege@gmail.com>
2023-06-17 02:43:39 -0400

    Product page has been updated

--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -1,9 +1,30 @@
 <template>
-    <h1>Products Page</h1>
+    <h1>Products</h1>
+    <div class="grid-wrap">
+      <!--in order to display a list in Vue ==> "v-for directive"-->
+      <div
+        class="product-item"
+        v-for="product in products"
+        :key="product.id"
+        >
+        <img :src="product.imageName" />
+        <h3 class="product-name">{{ product.name }}</h3>
+        <p class="product-price">{{ product.price }}</p>
+        <button>View Details</button>
+      </div>
+    </div>
 </template>
 
 <script>
+
+import products from '../temp-data'
+
 export default {
   name: 'ProductsPage',
+  data() {
+    return {
+      products,
+    }
+  }
 }
 </script>

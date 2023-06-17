23cf84018d0e11041c8729b70d73ef1697f78af0
arusafa <arusafacollege@gmail.com>
2023-06-17 04:30:04 -0400

    ProductList page has been added

---
+++ src/components/ProductsList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="grid-wrap">
+        <!--in order to display a list in Vue ==> "v-for directive"-->
+        <div class="product-item" v-for="product in products" :key="product.id">
+            <img :src="product.imageName" />
+            <h3 class="product-name">{{ product.name }}</h3>
+            <p class="product-price">{{ product.price }}</p>
+            <!-- in order to select the id we have to add : before the to-->
+            <!--<router-link :to="`/products/${product.id}`"> -->
+            <router-link :to="/products/ + product.id">
+                <button>View Details</button>
+            </router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProductsList',
+    props: ['products'],
+}
+</script>
--- src/pages/ProductsPage.vue
+++ src/pages/ProductsPage.vue
@@ -1,27 +1,19 @@
 <template>
   <h1>Products</h1>
-  <div class="grid-wrap">
-    <!--in order to display a list in Vue ==> "v-for directive"-->
-    <div class="product-item" v-for="product in products" :key="product.id">
-      <img :src="product.imageName" />
-      <h3 class="product-name">{{ product.name }}</h3>
-      <p class="product-price">{{ product.price }}</p>
-      <!-- in order to select the id we have to add : before the to-->
-      <!--<router-link :to="`/products/${product.id}`"> -->
-      <router-link :to="/products/ + product.id">
-        <button>View Details</button>
-      </router-link>
-
-    </div>
-  </div>
+  <!-- <ProductsList :products="products" comes from the ProductList's props /> -->
+  <ProductsList :products="products" />
 </template>
 
 <script>
 
+import ProductsList from '@/components/ProductsList.vue';
 import { products } from '../temp-data'
 
 export default {
   name: 'ProductsPage',
+  components: {
+    ProductsList,
+  },
   data() {
     return {
       products,
